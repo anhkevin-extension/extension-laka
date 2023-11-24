53d765d11386958c5393d59d9254aca348c675ca
Ánh Kevin <anh_kevin@hotmail.com>
2023-11-24 23:31:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log('main.js')
+
+injectContentCss(httpGet('/css/laka.css'));
+
+function injectContentCss(css) 
+{
+    var style = document.createElement('style');
+	  style.innerHTML = css;
+	  document.head.appendChild(style);
+}
