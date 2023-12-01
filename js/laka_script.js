79f35800a13dc3b1e58581ab175e1fb43f3f2150
Ánh Kevin <anh_kevin@hotmail.com>
2023-12-01 10:56:19 +0700

    Update laka_script.js

--- js/laka_script.js
+++ js/laka_script.js
@@ -8,5 +8,6 @@ console.log('Loaded laka_script.js');
     var iconChatElement = document.getElementById("chat-send-icon");
     var newIconChat = document.createElement("div");
     newIconChat.innerHTML ='<li id="_redmineInfo" class="_showDescription chatInput__redmineInfo" role="button" title="Get Redmine Task Subject"><span class="chatInput__iconContainer">【S】</span></span></li>';
+    newIconChat.innerHTML +='<li id="_redmineCC" class="_showDescription chatInput__redmineInfo" role="button" title="Get Redmine Task Related Members (To/CC)"><span class="chatInput__iconContainer">【C】</span></li>';
     iconChatElement.appendChild(newIconChat);
 })();
