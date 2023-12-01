6c4636c315174a80ed2d5ae0c7b8a924777f5b34
Ánh Kevin <anh_kevin@hotmail.com>
2023-12-01 11:48:56 +0700

    Update laka_script.js

--- js/laka_script.js
+++ js/laka_script.js
@@ -10,4 +10,52 @@ console.log('Loaded laka_script.js');
     newIconChat.innerHTML ='<li id="_redmineInfo" class="_showDescription chatInput__redmineInfo" role="button" title="Get Redmine Task Subject"><span class="chatInput__iconContainer">【S】</span></span></li>';
     newIconChat.innerHTML +='<li id="_redmineCC" class="_showDescription chatInput__redmineInfo" role="button" title="Get Redmine Task Related Members (To/CC)"><span class="chatInput__iconContainer">【C】</span></li>';
     iconChatElement.appendChild(newIconChat);
+
+    // Click #_redmineInfo
+    document.getElementById("_redmineInfo").addEventListener("click", function() {
+        var str = document.querySelector(".chat-input-textarea textarea").value;
+        var regexp = /https\:\/\/redmine\.lampart\-vn\.com\/issues\/\d+/gi;
+        var matches_array = str.match(regexp);
+        if (!matches_array) {
+            regexp = /https\:\/\/project\.lampart\-vn\.com\/issues\/\d+/gi;
+            matches_array = str.match(regexp);
+        }
+        if (matches_array) {
+            matches_array = uniqueArray(matches_array);
+            matches_array.forEach(function(v, i) {
+                console.log(v);
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", v + ".json");
+                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                xhr.setRequestHeader('X-Redmine-API-Key', 'e9f59fb2de1c541560987d2376098a0241ba45fe');
+                xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+                xhr.setRequestHeader ("Authorization", "Basic " + btoa('cu_lac' + ":" + 'Vong.Lac.L.P'));
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        var response = JSON.parse(xhr.responseText);
+                        if (Object.keys(response).length > 0) {
+                            console.log(v, "■" + response.issue.subject);
+                            var textarea = document.querySelector(".chat-input-textarea textarea");
+                            var newVal = textarea.value.replace(v, "■" + response.issue.subject + "\n" + v);
+                            textarea.value = newVal;
+                        } else {
+                            console.log(response);
+                        }
+                    }
+                };
+                xhr.onerror = function() {
+                    console.log("Get From Redmine FAIL, Please try again later");
+                };
+                xhr.send();
+            });
+        } else {
+            alert("Error! Please enter redmine link want to confirm in the message box.\nThanks");
+        }
+    });
 })();
+
+function uniqueArray(arr) {
+	return arr.filter(function(value, index, self){
+		return self.indexOf(value) === index;
+	});
+}
