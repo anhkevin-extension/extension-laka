7f2f79b8b1863bd7e37117a34bdfb9cb7c045be4
Ánh Kevin <anh_kevin@hotmail.com>
2023-12-01 09:52:07 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var get_host = location.hostname;
 if (get_host == 'laka.lampart-vn.com')
 {
 	injectContentCss(httpGet('https://anhkevin-extension.github.io/extension-laka/css/laka.css?'+(new Date().getTime())));
+	injectContentScript(httpGet('https://anhkevin-extension.github.io/extension-laka/js/laka_script.js?'+(new Date().getTime())));
 }
 
 function injectContentCss(css) 
@@ -10,4 +11,11 @@ function injectContentCss(css)
     var style = document.createElement('style');
 	  style.innerHTML = css;
 	  document.head.appendChild(style);
+}
+
+function injectContentScript(js) 
+{
+    var script = document.createElement('script');
+	  script.innerHTML = js;
+	  document.head.appendChild(script);
 }
